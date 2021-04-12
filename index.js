48bd5b40cc28e353b19a9cf363e0ae18be0f4c7b
Nick Flores-Tober <nflores161@gmail.com>
2021-04-11 23:00:09 -0400

    commit tree 37fbb085723ba0a6b0abf4eb1e5d0a1498b634f4

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(pickupLocation) {
+  let blocks 
+  if (pickupLocation >= 42) {
+    blocks = pickupLocation - 42;
+  } else if (pickupLocation <= 42) {
+    blocks = 42 - pickupLocation;
+  } return blocks;
+};
+
+function distanceFromHqInFeet(startBlock) {
+  distanceFromHqInBlocks(startBlock);
+  return blocks;
+  let feet;
+  feet = blocks * 264;
+  return feet;
+}
